dadd129b40a03bd1c3c889c0ae4142c605e7c6d7
Mikolas-MBPro <stuchlej@gmail.com>
2018-08-31 11:59:11 +0200

    Create pretty print test

--- Tests/XMLParsingTests/XMLParsingTests.swift
+++ Tests/XMLParsingTests/XMLParsingTests.swift
@@ -2,15 +2,47 @@ import XCTest
 @testable import XMLParsing
 
 class XMLParsingTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(XMLParsing().text, "Hello, World!")
+
+    struct SomeData: Codable {
+        let myArray: [Int]
+        let myBool: Bool
+        let myNames: [String]
+        let nested: SomeData2?
     }
 
+    struct SomeData2: Codable {
+        let myArray: [Int]
+        let myBool: Bool
+        let myNames: [String]
+        let nested: SomeData3?
+    }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    struct SomeData3: Codable {
+        let myArray: [Int]
+        let myBool: Bool
+        let myNames: [String]
+    }
+
+    let dummyData: SomeData = SomeData(myArray: [15,1,198,-156,-4], myBool: true, myNames: ["Hi","Miki","Whatever"], nested: SomeData2(myArray: [-156,-4], myBool: true, myNames: ["Hi","adsf","fddf"], nested: SomeData3(myArray: [15,1,198], myBool: true, myNames: ["asas","Miki","Whatever"])))
+
+    func testEncodePretty() {
+        let xmlEncoder = XMLEncoder()
+        xmlEncoder.outputFormatting.update(with: .prettyPrinted)
+        xmlEncoder.outputFormatting.update(with: .sortedKeys)
+
+        let encoded = try! xmlEncoder.encode(dummyData, withRootKey: "xml-dummy")
+        let string = String(data: encoded, encoding: .utf8)!
+
+        print(string)
+    }
+
+    func testEncodeNonPretty() {
+        let xmlEncoder = XMLEncoder()
+
+        let encoded = try! xmlEncoder.encode(dummyData, withRootKey: "xml-dummy")
+        let string = String(data: encoded, encoding: .utf8)!
+
+        print(string)
+    }
+
 }
